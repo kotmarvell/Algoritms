dc59e88f58c8955187713c09bdd91ea3517ec068
kotmarvell <53427789+kotmarvell@users.noreply.github.com>
2019-12-02 00:38:26 +0300

    new : add bubble sorting for integer

---
+++ Alg/Alg.Sorting/BubbleSorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alg.Sorting
+{
+    public static class BubbleSorting
+    {
+        public static int[] BubbleSortInt(int[] array)
+        {
+            int length = array.Length;
+            for(int i = 0; i < length - 1; i++)
+            {
+                for(int l = i; l < length; l++)
+                {
+                    if(array[i] > array[l])
+                    {
+                        Swap(array[i], array[l]);
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        private static void Swap(int x, int y)
+        {
+            int swap = x;
+            x = y;
+            y = swap;
+        }
+    }
+}
