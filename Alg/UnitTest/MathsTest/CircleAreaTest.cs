b75879712592803ed5d610124f220244066116e1
kotmarvell <53427789+kotmarvell@users.noreply.github.com>
2019-12-01 22:44:12 +0300

    new : UnitTest (add area and perimetr test)

---
+++ Alg/UnitTest/MathsTest/CircleAreaTest.cs
@@ -0,0 +1,32 @@
+﻿using Alg.Maths.Area;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.MathsTest
+{
+    [TestClass]
+    public class CircleAreaTest
+    {
+        [TestMethod]
+        public void CircleAreaForRTest()
+        {
+            double R = 2;
+            double area = 4 * 3.14;
+
+            var res = CircleArea.CircleAreaForR(R);
+            Assert.IsTrue(area == res);
+        }
+
+        [TestMethod]
+        public void CircleAreaForDTest()
+        {
+            double D = 4;
+            double area = 4 * 3.14;
+
+            var res = CircleArea.CircleAreaForD(D);
+            Assert.IsTrue(area == res);
+        }
+    }
+}
